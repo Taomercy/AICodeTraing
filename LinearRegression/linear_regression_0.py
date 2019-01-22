f76e0db1793e8082ca44c3398c8072c1ad4e3b86
taomercy <taomercy@qq.com>
2019-01-22 19:46:05 +0800

    c1 c2

--- LinearRegression/linear_regression_0.py
+++ LinearRegression/linear_regression_0.py
@@ -3,60 +3,94 @@
 from numpy import *
 import matplotlib.pylab as plt
 import sympy
-import time
 
-'''
 data = array([
     [1.2, 3.6],
     [2.3, 4.6],
     [1.8, 7.6],
     [5.4, 15.8],
 ])
-'''
 
-def plotting(data, k, b):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.scatter(array(data[:, 0]), array(data[:, 1]))
-    x = arange(0, 10, 0.01)
-    y = curve(x, k, b)
-    ax.plot(x, y, 'r')
-    plt.show()
+#data = loadtxt('data.csv', delimiter=',')
 
 
-def curve(x, k, b):
+def curve1(x, k, b):
     return k*x + b
 
 
-def main():
-    start = time.time()
-
-    # set y = kx + b
-    data = loadtxt('data.csv', delimiter=',')
+def main1():
     k = sympy.Symbol('k')
     b = sympy.Symbol('b')
+
+    def plotting(paras):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(array(data[:, 0]), array(data[:, 1]))
+        x = arange(0, 10, 0.01)
+        y = curve1(x, paras[k], paras[b])
+        ax.plot(x, y, 'r')
+        plt.show()
+
+    # set y = kx + b
     loss = sympy.Symbol('loss')
     for i in data:
-        loss += (i[1] - (k*i[0] + b))**2
-    
+        loss += (i[1] - curve1(i[0], k, b)) ** 2
+
     dlossdk = sympy.diff(loss, k)
     dlossdb = sympy.diff(loss, b)
-    
+
     # 对k求偏导数
     print "dlossdk:", dlossdk
     # 对b求偏导数
     print "dlossdb:", dlossdb
-    
+
     # 联立方程组求解
     res = sympy.solve([dlossdk, dlossdb], [k, b])
     print "k =", res[k]
     print "v =", res[b]
     # 作图
-    end = time.time()
-    print "use time:", end -start
+    plotting(res)
 
-    plotting(data, res[k], res[b])
+
+def curve2(x, a, b, c):
+    return a*x**2 + b*x + c
+
+
+def main2():
+    a, b, c = sympy.symbols('a b c')
+    
+    def plotting(paras):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(array(data[:, 0]), array(data[:, 1]))
+        x = arange(0, 10, 0.01)
+        y = curve2(x, paras[a], paras[b], paras[c])
+        ax.plot(x, y, 'r')
+        plt.show()
+
+    # set y = ax**2 + kx +b
+    loss = sympy.Symbol('loss')
+    for i in data:
+        loss += (i[1] - curve2(i[0], a, b, c))**2
+    
+    dlossda = sympy.diff(loss, a)
+    dlossdb = sympy.diff(loss, b)
+    dlossdc = sympy.diff(loss, c)
+    
+    print "dlossda:", dlossda
+    print "dlossdb:", dlossdb
+    print "dlossdc:", dlossdc
+    
+    # 联立方程组求解
+    res = sympy.solve([dlossda, dlossdb, dlossdc], [a, b, c])
+    print "a =", res[a]
+    print "b =", res[b]
+    print "c =", res[c]
+    # 作图
+    plotting(res)
 
 
 if __name__ == '__main__':
-    main()
+    main1()
+    main2()
+
