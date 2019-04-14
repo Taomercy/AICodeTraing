bec5cf3232c63206bcf1b0265d7230fd38730b3a
Taomercy <35693926+Taomercy@users.noreply.github.com>
2019-04-14 16:19:25 +0800

    add expx

---
+++ expx.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def expx(x):
+    y = np.ones_like(x)
+    i = x > 0
+    y[i] = np.power(x[i], x[i])
+    i = x < 0
+    y[i] = np.power(-x[i], -x[i])
+    return y
+
+
+def expx_main():
+    x = np.linspace(-1.5, 2, 1000)
+    y = expx(x)
+    plt.plot(x, y, 'b-', label='x^x', linewidth=2)
+    plt.grid(True)
+    plt.legend(loc='upper left')
+    plt.show()
+
+
+def lnx(x):
+    return np.log(x)
+
+
+def lnx_main():
+    x = np.linspace(0, 5, 1000)
+    y = lnx(x)
+    plt.plot(x, y, 'b-', label='lnx', linewidth=2)
+    plt.grid(True)
+    plt.legend(loc='upper left')
+    plt.show()
+
+
+if __name__ == '__main__':
+    lnx_main()
