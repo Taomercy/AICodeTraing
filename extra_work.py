293cee0128f8286290b4fcd0c39a677c8e082675
taomercy <taomercy@qq.com>
2021-10-11 17:50:23 +0800

    2

--- extra_work.py
+++ extra_work.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import numpy
 import sympy
 from numpy import array, arange
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 gdp = [22460, 11226, 34547, 4851, 5444, 2662, 4549]
 consume = [7326, 4490, 11546, 2396, 2208, 1608, 2035]
@@ -34,23 +36,32 @@ def main():
     dlossdk = sympy.diff(loss, k)
     dlossdb = sympy.diff(loss, b)
 
-    # 对k求偏导数
-    print("dlossdk:", dlossdk)
-    # 对b求偏导数
-    print("dlossdb:", dlossdb)
-
     # 联立方程组求解
     res = sympy.solve([dlossdk, dlossdb], [k, b])
     value_k = float(res[k])
     value_b = float(res[b])
-    print("k =", res[k], value_k)
-    print("v =", res[b], value_b)
+    print("线性回归方程：y={}*x+{}".format(value_k, value_b))
     # 作图
     plotting(res)
 
+    # 显著性检验
+    predicts = [i*value_k+value_b for i in gdp]
+    # 回归平方和
+    regression = sum((predicts-numpy.mean(consume))**2)
+    print("回归平方和(RSS):", regression)
+    # 残差平方和
+    residual = sum((array(consume)-predicts)**2)
+    print("残差平方和(ESS):", residual)
+    # 总体平方和
+    total = sum((consume-numpy.mean(consume))**2)
+    print("总体平方和(TSS):", residual)
+    # 相关系数 R^2
+    r_square = 1-residual/total
+    print("相关系数(R^2):", r_square)
+
     # when x = 5000
     value = value_k*5000 + value_b
-    print("when gdp= 5000, consume=", value)
+    print("当 gdp= 5000, consume=", value)
 
 
 if __name__ == '__main__':
