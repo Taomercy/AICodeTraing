ab4318148357b97a38efce9b2561e8839c5ae355
Taomercy <taomercy@qq.com>
2021-10-12 00:09:18 +0800

    1

--- pattern_recognition/footprint2.py
+++ pattern_recognition/footprint2.py
@@ -3,16 +3,18 @@
 from pylab import *
 from PIL import Image
 
-image_name = "footprint.jpg"
+image_name = "footprint1.jpg"
+# image_name = "footprint.jpg"
 gray = Image.open(image_name).convert('1')
+# gray = Image.open(image_name).convert('L')
 plt.imshow(gray, cmap=plt.cm.gray_r)
 plt.axis('off')
 plt.show()
 
 
 img = array(gray)
-print(img)
-contour = plt.contour(img, 3, colors='black', linewidths=0.5)
-plt.clabel(contour, fontsize=10, colors=('k', 'r'))
+# contour = plt.contour(img, 3, colors='black', linewidths=0.5)
+contour = plt.contour(img, 5, cmap=plt.cm.hot)
+# plt.clabel(contour, fontsize=10, colors=('k', 'r'))
 plt.axis('off')
 plt.show()
