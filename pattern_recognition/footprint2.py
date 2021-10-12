b5b488b5cfdef02a9227cbb920499a46d2c3dcb3
taomercy <taomercy@qq.com>
2021-10-12 14:30:58 +0800

    footprint

--- pattern_recognition/footprint2.py
+++ pattern_recognition/footprint2.py
@@ -8,14 +8,35 @@ from PIL import Image
 
 
 # convert 参数主要选项: "L", "1"
-# camp 参数主要选项: "plt.cm.hot"(热力图)
+# camp 参数主要选项: "hot"(热力图), "PRGn", "rainbow"
 # clabel参数为是否标注等高线数值
+"""
+cmap的候选值有
+'Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 
+'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 
+'Oranges_r','PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 
+'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 
+'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r',
+ 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Vega10', 'Vega10_r', 'Vega20', 'Vega20_r', 'Vega20b', 
+ 'Vega20b_r', 'Vega20c', 'Vega20c_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 
+ 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 
+ 'brg', 'brg_r', 'bwr', 'bwr_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 
+ 'cubehelix_r', 'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_heat', 'gist_heat_r',
+  'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 
+  'gist_yarg_r', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', 'hot_r', 'hsv', 'hsv_r', 
+  'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 
+  'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', 'seismic_r', 
+  'spectral', 'spectral_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 
+  'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'viridis', 'viridis_r', 'winter', 'winter_r'
+"""
+
+
 def footprint_recognition(image_file, convert="L", camp=None, clabel=True):
     gray = Image.open(image_file).convert(convert)
     img = array(gray)
     contour = plt.contour(img, 3, colors='black', linewidths=0.5)
     if camp:
-        contour = plt.contour(img, 5, cmap=camp)
+        contour = plt.contour(img, 10, cmap=camp)
     if clabel:
         plt.clabel(contour, fontsize=10, colors=('k', 'r'))
     plt.axis('off')
@@ -30,7 +51,7 @@ def main():
     input_dir = "image_input"
     images = [os.path.join(input_dir, i) for i in os.listdir(input_dir)]
     for image_file in images:
-        footprint_recognition(image_file, convert="L", camp=plt.cm.hot, clabel=False)
+        footprint_recognition(image_file, convert="L", camp=plt.cm.rainbow, clabel=False)
         # footprint_recognition(image_file, convert="L", camp=None, clabel=False)
 
 
